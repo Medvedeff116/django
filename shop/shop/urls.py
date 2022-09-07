9c45b7c691ff73067fbec2a3135356a78b9fe9ba
Medvedeff116 <frost19971@yandex.ru>
2022-09-07 22:37:56 +0300

    add model

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
+    path('swagger-ui/', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url': 'api_schema'}),name='swagger-ui'),
+    path('api_schema/', get_schema_view(title='Project API ', description='REST API'), name='api_schema'),
+    path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
 ]
